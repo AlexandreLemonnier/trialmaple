2ff490ecb3b8579cae352e6d019f952bdfaf2f10
Alexandre LEMONNIER <alexandre.lemonnier@outlook.com>
2025-08-16 14:24:20 +0200

    Use record for DTO

--- backend/src/main/java/com/trialmaple/model/dto/DailyStatsDto.java
+++ backend/src/main/java/com/trialmaple/model/dto/DailyStatsDto.java
@@ -1,19 +1,6 @@
 package com.trialmaple.model.dto;
 
-public class DailyStatsDto {
-    private int nbWinners;
-    private double averageTries;
-
-    public DailyStatsDto(int nbWinners, double averageTries) {
-        this.nbWinners = nbWinners;
-        this.averageTries = averageTries;
-    }
-
-    public int getNbWinners() {
-        return nbWinners;
-    }
-
-    public double getAverageTries() {
-        return averageTries;
-    }
+public record DailyStatsDto(
+        int nbWinners,
+        double averageTries) {
 }
--- backend/src/main/java/com/trialmaple/model/dto/ErrorResponseDto.java
+++ backend/src/main/java/com/trialmaple/model/dto/ErrorResponseDto.java
@@ -2,26 +2,8 @@ package com.trialmaple.model.dto;
 
 import java.time.Instant;
 
-public class ErrorResponseDto {
-    private Instant timestamp;
-    private String error;
-    private String message;
-
-    public ErrorResponseDto(Instant timestamp, String error, String message) {
-        this.timestamp = timestamp;
-        this.error = error;
-        this.message = message;
-    }
-
-    public Instant getTimestamp() {
-        return timestamp;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+public record ErrorResponseDto(
+        Instant timestamp,
+        String error,
+        String message) {
 }
