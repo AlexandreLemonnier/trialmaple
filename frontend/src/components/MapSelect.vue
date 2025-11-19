7d4d4514b3285a5f7fceb58db3b070f66cf6877d
Alexandre LEMONNIER <alexandre.lemonnier@outlook.com>
2025-11-19 22:50:19 +0100

    scroll to select list with keyboard using first letter

--- frontend/src/components/MapSelect.vue
+++ frontend/src/components/MapSelect.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="relative w-full flex-1 min-w-0">
+  <div class="relative w-full flex-1 min-w-0" @keydown="onKeyDown">
     <button
       @click="isOpen = !isOpen"
-      class="flex w-full justify-between items-center text-lg lg:text-2xl rounded-full border-2 py-2 px-4"
+      class="flex w-full justify-between items-center text-lg lg:text-2xl rounded-full border-2 py-2 px-4 cursor-pointer"
     >
       <span class="truncate">{{ selectedMap || "Select a map..." }}</span>
       <Icon :name="isOpen ? 'chevron-up' : 'chevron-down'" size="sm" />
@@ -12,14 +12,15 @@
       v-if="isOpen"
       class="absolute text-md lg:text-lg mt-2 w-full max-h-60 overflow-y-auto rounded-xl border shadow-lg bg-white scrollbar-hide"
     >
-      <li
-        v-for="mapName in mapNames"
-        :key="mapName"
-        @click="select(mapName)"
-        class="px-4 py-1 hover:bg-neutral-400/20 cursor-pointer"
-      >
-        {{ mapName }}
-      </li>
+    <li
+      v-for="mapName in mapNames"
+      :key="mapName"
+      @click="select(mapName)"
+      class="px-4 py-1 hover:bg-neutral-400/20 cursor-pointer"
+      ref="itemRefs"
+    >
+      {{ mapName }}
+    </li>
     </ul>
   </div>
 </template>
@@ -28,15 +29,31 @@
 import Icon from '#/components/Icon.vue';
 import { ref } from 'vue';
 
-defineProps<{
+const { mapNames } = defineProps<{
     mapNames: string[];
 }>();
 
 const isOpen = ref(false);
 const selectedMap = defineModel<string>();
+const itemRefs = ref<(HTMLElement | null)[]>([]);
 
 function select(mapName: string) {
   selectedMap.value = mapName;
   isOpen.value = false;
 }
+
+function onKeyDown(e: KeyboardEvent) {
+  if (e.key.length !== 1 || !/[a-z]/i.test(e.key)) return;
+
+  const letter = e.key.toLowerCase();
+  const index = mapNames.findIndex(name =>
+    name.toLowerCase().startsWith(letter)
+  );
+
+  if (index !== -1) {
+    const el = itemRefs.value[index];
+    el?.scrollIntoView({ block: "start", behavior: "instant" });
+  }
+}
+
 </script>
