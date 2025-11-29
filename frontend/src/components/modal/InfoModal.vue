6c0cf870de60536d83176db51ae8e930516b5af5
Alexandre LEMONNIER <alexandre.lemonnier@outlook.com>
2025-11-29 21:13:39 +0100

    Info modal

---
+++ frontend/src/components/modal/InfoModal.vue
@@ -0,0 +1,20 @@
+<template>
+    <Modal title="About TrialMaple" v-model="isOpen">
+        <div class="flex flex-col gap-2 mt-3 text-sm text-neutral-300">
+            <p><strong>TrialMaple is a daily guessing game</strong> based on TMNF's "<a href="https://tmrpgtrial.com/list/cm7z509dr0f91mph5e47sftz0" target="_blank"><u>Hardest Trials List</u></a>".</p>
+            <p>Note that <strong>unfinished maps</strong> are not included.</p>
+            <hr class="opacity-20" />
+            <p>Pick a map and click <strong>"Guess"</strong> to get hints: does today's map have more or fewer points? Which authors match? Repeat until you find it!</p>
+            <hr class="opacity-20" />
+            <p>A new map becomes available every day at <strong>00:00 CEST</strong>.</p>
+            <p>Maps data last updated on <strong>12 October 2025</strong>.</p>
+        </div>
+    </Modal>
+</template>
+
+<script setup lang="ts">
+import Modal from '#/components/modal/Modal.vue';
+
+const isOpen = defineModel<boolean>();
+
+</script>
---
+++ frontend/src/components/modal/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+    <Transition name="modal">
+        <div v-if="isModalOpen" class="fixed inset-0 z-9999 flex items-center justify-center bg-black/60 p-4" @click.self="isModalOpen = false">
+            <div class="bg-neutral-900 text-neutral-100 rounded-2xl p-5 max-w-md w-full border border-white/10 shadow-xl transition-all duration-150 transform">
+                <div class="flex items-start justify-between gap-4">
+                    <h2 class="text-lg font-black">{{ title }}</h2>
+                    <Icon class="cursor-pointer" name="x" size="sm" @click="isModalOpen = false" />
+                </div>
+                <slot></slot>
+            </div>
+        </div>
+    </Transition>
+</template>
+
+<script setup lang="ts">
+import Icon from '#/components/Icon.vue';
+
+defineProps<{
+    title: string;
+}>();
+
+const isModalOpen = defineModel<boolean>();
+</script>
