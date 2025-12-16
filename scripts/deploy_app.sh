9ae7d70724b5321fb140c729f8fc112e5aeb74f1
dev <dev@vps-3fa65db1.vps.ovh.net>
2025-12-16 17:51:48 +0100

    add deploy_app script

---
+++ scripts/deploy_app.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Update sources
+cd /home/dev/trialmaple
+git pl
+#Build backend
+cd /home/dev/trialmaple/backend
+mvn clean package
+cp target/trialmaple.jar /home/dev/app/trialmaple.jar
+sudo systemctl restart trialmaple
+# Build frontend
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+cd /home/dev/trialmaple/frontend
+nvm i
+nvm use
+npm run build
+sudo cp -r dist/* /var/www/trialmaple
