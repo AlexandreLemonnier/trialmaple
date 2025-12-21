431b9c80da385cb2d7e58371fdfefc9e6e2e7788
Alexandre LEMONNIER <alexandre.lemonnier@outlook.com>
2025-12-21 15:27:04 +0100

    Revert "log IP"
    This reverts commit abd00063fa58c8808943d83afe96882074f8219d.

--- backend/src/main/java/com/trialmaple/controller/GuessController.java
+++ backend/src/main/java/com/trialmaple/controller/GuessController.java
@@ -1,7 +1,5 @@
 package com.trialmaple.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,14 +15,10 @@ import com.trialmaple.model.entities.DailyMap;
 import com.trialmaple.service.DailyMapService;
 import com.trialmaple.service.GuessService;
 
-import jakarta.servlet.http.HttpServletRequest;
-
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "*")
 public class GuessController {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(GuessController.class);
 
     private final DailyMapService dailyMapService;
     private final GuessService guessService;
@@ -35,15 +29,8 @@ public class GuessController {
     }
 
     @PostMapping("/guess")
-    public GuessDto guess(@RequestBody GuessRequestDto request, HttpServletRequest httpRequest)
+    public GuessDto guess(@RequestBody GuessRequestDto request)
             throws NoDailyMapFoundException, InvalidMapNameException {
-        // temporary code
-        LOGGER.info("Guess from IP : " + httpRequest.getRemoteAddr());
-        String xff = httpRequest.getHeader("X-Forwarded-For");
-        if (xff != null && !xff.isEmpty()) {
-            LOGGER.info("IP through XFF : " + xff.split(",")[0].trim());
-        }
-        // end temporary code
         DailyMap dailyMap = dailyMapService.getCurrentDailyMap();
         return guessService.checkGuess(dailyMap, request);
     }
