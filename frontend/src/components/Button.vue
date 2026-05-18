edd9a0522884f75c86c321a38a757ba155348cef
Alexandre LEMONNIER <alexandre.lemonnier@outlook.com>
2026-05-18 21:50:12 +0200

    add will-change: transform to button

--- frontend/src/components/Button.vue
+++ frontend/src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button type="button" class="flex items-center gap-1.5 lg:gap-2 text-sm lg:text-base rounded-full border border-app-border py-1 px-3 bg-button cursor-pointer hover:scale-105 transition-transform">
+    <button type="button" class="flex items-center gap-1.5 lg:gap-2 text-sm lg:text-base rounded-full border border-app-border py-1 px-3 bg-button cursor-pointer hover:scale-105 transition-transform will-change-transform">
         <Icon v-if="iconName && iconPosition === 'left'" :class="iconClass" :name="iconName" :size="iconSize" />
         <span v-if="label">{{ label }}</span>
         <Icon v-if="iconName && iconPosition === 'right'" :class="iconClass" :name="iconName" :size="iconSize" />
