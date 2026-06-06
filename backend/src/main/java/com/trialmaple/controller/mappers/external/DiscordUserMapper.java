a2abd48d8714499e80c66d1d4a5843227f8e9daa
Alexandre LEMONNIER <alexandre.lemonnier@outlook.com>
2026-06-06 14:31:24 +0200

    use global_name from discord instead of username if defined

--- backend/src/main/java/com/trialmaple/controller/mappers/external/DiscordUserMapper.java
+++ backend/src/main/java/com/trialmaple/controller/mappers/external/DiscordUserMapper.java
@@ -1,5 +1,6 @@
 package com.trialmaple.controller.mappers.external;
 
+import ch.qos.logback.core.util.StringUtil;
 import com.trialmaple.model.dto.external.discord.DiscordUserDto;
 import com.trialmaple.model.entities.User;
 import org.springframework.stereotype.Component;
@@ -7,9 +8,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class DiscordUserMapper {
     public User externalToService(DiscordUserDto discordUser) {
+        String name = StringUtil.isNullOrEmpty(discordUser.globalName()) ? discordUser.username() : discordUser.globalName();
         return new User(
                 Long.parseLong(discordUser.id()),
-                discordUser.username(),
+                name,
                 discordUser.avatar(),
                 discordUser.discriminator()
         );
--- backend/src/main/java/com/trialmaple/model/dto/external/discord/DiscordUserDto.java
+++ backend/src/main/java/com/trialmaple/model/dto/external/discord/DiscordUserDto.java
@@ -1,8 +1,11 @@
 package com.trialmaple.model.dto.external.discord;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record DiscordUserDto(
         String id,
         String username,
+        @JsonProperty("global_name") String globalName,
         String avatar,
         String discriminator) {
 }
