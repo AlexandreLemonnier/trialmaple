a22f222b553dab756eb43effa985e06c4ce86af7
Alexandre LEMONNIER <alexandre.lemonnier@outlook.com>
2026-07-16 23:19:57 +0200

    add backoffice build to deploy script

--- scripts/deploy_app.sh
+++ scripts/deploy_app.sh
@@ -2,18 +2,31 @@
 # Update sources
 cd /home/dev/trialmaple
 git pl
+
 #Build backend
 cd /home/dev/trialmaple/backend
 mvn clean package
 cp target/trialmaple.jar /home/dev/app/trialmaple.jar
 sudo systemctl restart trialmaple
+
 # Build frontend
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 cd /home/dev/trialmaple/frontend
 nvm i
 nvm use
 npm i
 npm run build
+sudo rm -rf /var/www/trialmaple/*
 sudo cp -r dist/* /var/www/trialmaple
+
+# Build backoffice
+cd /home/dev/trialmaple/backoffice
+nvm i
+nvm use
+npm i
+npm run build
+sudo rm -rf /var/www/admin_trialmaple/*
+sudo cp -r dist/* /var/www/admin_trialmaple
