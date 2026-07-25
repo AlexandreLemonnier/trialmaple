51866dda3bfd40ab0ee951f78926218cd6972af3
Alexandre LEMONNIER <alexandre.lemonnier@outlook.com>
2026-07-25 17:26:13 +0200

    make maps update and daily map pick async when starting the app

--- backend/src/main/java/com/trialmaple/dailymap/jobs/DailyMapInitializer.java
+++ backend/src/main/java/com/trialmaple/dailymap/jobs/DailyMapInitializer.java
@@ -1,12 +1,12 @@
 package com.trialmaple.dailymap.jobs;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-
 import com.trialmaple.dailymap.DailyMapService;
 import com.trialmaple.tmmap.TmMapService;
-
-import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
@@ -15,7 +15,8 @@ public class DailyMapInitializer {
     private final DailyMapService dailyMapService;
     private final TmMapService tmMapService;
 
-    @PostConstruct
+    @EventListener(ApplicationReadyEvent.class)
+    @Async
     public void init() {
         tmMapService.fetchAndUpdateMaps();
         dailyMapService.pickAllDailyMapsIfMissing();
